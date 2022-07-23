8d4f4adab2e4f1f4533880e25493d72bae4b5da6
azamatos <87179356+azamatos@users.noreply.github.com>
2022-07-23 12:09:20 +0000

    Deploying to gh-pages from @ azamatos/punk_api@54adec93f612def745f5a938b12938e2bf26d21c 🚀

---
+++ _next/static/AQMq5aVI5ZuOImHG3kj67/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7ed42b014b5dbe02.css","static/chunks/pages/index-ca8bfa4c302c9015.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
